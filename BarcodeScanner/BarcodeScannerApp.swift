688b5fef7bdac864b4eb0b092149eec5f6cf9cce
Marko Kramar <marko.kramar7@gmail.com>
2023-12-26 20:33:37 +0100

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Marko Kramar on 26.12.2023..
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
