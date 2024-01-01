82d5f4b1f795588f2f8ff417118c8429f90e55a8
Marko Kramar <marko.kramar7@gmail.com>
2024-01-01 11:15:59 +0100

    Initial commit

---
+++ BarcodeScanner/ScannerVC.swift
@@ -0,0 +1,114 @@
+//
+//  ScannerVC.swift
+//  BarcodeScanner
+//
+//  Created by Marko Kramar on 26.12.2023..
+//
+
+import UIKit
+import AVFoundation
+
+enum CameraError: String {
+    case invalidDeviceInput = "Something is wrong with the camera. We are unable to capture the input."
+    case invalidScanValue = "The value scanned is not valid. This app scanes EAN-8 and EAN-13."
+}
+
+protocol ScannerVCDelegate: AnyObject {
+    func didFind(barcode: String)
+    func didSurface(error: CameraError)
+}
+
+final class ScannerVC: UIViewController {
+    
+    let captureSession = AVCaptureSession()
+    var previewLayer: AVCaptureVideoPreviewLayer?
+    weak var scannerDelegate: ScannerVCDelegate!
+    
+    init(scannerDelegate: ScannerVCDelegate) {
+        super.init(nibName: nil, bundle: nil)
+        self.scannerDelegate = scannerDelegate
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupCaptureSession()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        guard let previewLayer = previewLayer else {
+            scannerDelegate.didSurface(error: .invalidDeviceInput)
+            return
+        }
+        
+        previewLayer.frame = view.layer.bounds
+    }
+    
+    private func setupCaptureSession() {
+        guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else {
+            scannerDelegate.didSurface(error: .invalidDeviceInput)
+            return
+        }
+        
+        let videoInput: AVCaptureDeviceInput
+        do {
+            try videoInput = AVCaptureDeviceInput(device: videoCaptureDevice)
+        } catch {
+            scannerDelegate.didSurface(error: .invalidDeviceInput)
+            return
+        }
+        
+        if captureSession.canAddInput(videoInput) {
+            captureSession.addInput(videoInput)
+        } else {
+            scannerDelegate.didSurface(error: .invalidDeviceInput)
+            return
+        }
+        
+        let metaDataOutput = AVCaptureMetadataOutput()
+        if captureSession.canAddOutput(metaDataOutput) {
+            captureSession.addOutput(metaDataOutput)
+            metaDataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            metaDataOutput.metadataObjectTypes = [.ean8, .ean13]
+        } else {
+            scannerDelegate.didSurface(error: .invalidDeviceInput)
+            return
+        }
+        
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer!.videoGravity = .resizeAspectFill
+        view.layer.addSublayer(previewLayer!)
+        
+        captureSession.startRunning()
+    }
+    
+}
+
+extension ScannerVC: AVCaptureMetadataOutputObjectsDelegate {
+    
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        guard let object = metadataObjects.first else {
+            scannerDelegate?.didSurface(error: .invalidScanValue)
+            return
+        }
+        
+        guard let machineReadableObject = object as? AVMetadataMachineReadableCodeObject else {
+            scannerDelegate?.didSurface(error: .invalidScanValue)
+            return
+        }
+        
+        guard let barcode = machineReadableObject.stringValue else {
+            scannerDelegate?.didSurface(error: .invalidScanValue)
+            return
+        }
+        
+        captureSession.stopRunning()
+        scannerDelegate.didFind(barcode: barcode)
+    }
+    
+}
